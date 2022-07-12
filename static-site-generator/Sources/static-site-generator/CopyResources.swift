a2a52a5477b2283474b071262414815340c654d9
Ido Mizrachi <idomizrachi@gmail.com>
2022-07-12 09:35:49 +0300

    Fix copy resources

--- static-site-generator/Sources/static-site-generator/CopyResources.swift
+++ static-site-generator/Sources/static-site-generator/CopyResources.swift
@@ -13,15 +13,18 @@ struct CopyResources {
     
     func run() throws {
         do {
-            FileManager.default.createDirectory(at: URL(fileURLWithPath: buildPath), withIntermediateDirectories: true)
-        } catch {            
+            print("Create folder \(buildPath)")
+            try FileManager.default.createDirectory(at: URL(fileURLWithPath: buildPath), withIntermediateDirectories: true)
+        } catch {
         }
         let resouceFiles = try FileManager.default.contentsOfDirectory(atPath: templatesResourcesPath)
         try resouceFiles.forEach { resourceFile in
             let buildResourceFile = buildPath + "/" + resourceFile
             if FileManager.default.fileExists(atPath: buildResourceFile) {
+                print("Deleting file \(buildResourceFile)")
                 try FileManager.default.removeItem(atPath: buildResourceFile)
             }
+            print("Copying file \(templatesResourcesPath)/\(resourceFile) to \(buildResourceFile)")
             try FileManager.default.copyItem(atPath: templatesResourcesPath + "/" +  resourceFile, toPath: buildResourceFile)
         }
     }
